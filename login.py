08941be131f9c54f665daf81d9c4fc5bf7fa6b66
chronomeister <chronomeista@gmail.com>
2017-06-22 16:51:39 -0500

    got api working (dont know what to do when under maint)

--- login.py
+++ login.py
@@ -1,14 +1,16 @@
-import configparser, os, requests, io, re, pprint
+import configparser, os, requests, io, re, pprint, json, time, math
 config = configparser.ConfigParser()
 config.read('configs.ini')
+
+servers = ("203.104.209.71", "203.104.209.87", "125.6.184.16", "125.6.187.205", "125.6.187.229", "125.6.187.253", "125.6.188.25", "203.104.248.135", "125.6.189.7", "125.6.189.39", "125.6.189.71", "125.6.189.103", "125.6.189.135", "125.6.189.167", "125.6.189.215", "125.6.189.247", "203.104.209.23", "203.104.209.39", "203.104.209.55", "203.104.209.102")
 
 s = requests.Session()
 # print(config.get('dmm', 'user'))
 # print(config.get('dmm', 'pass'))
-baselogin = requests.get('https://www.dmm.com/my/-/login/=/path=Sg9VTQFXDFcXFl5bWlcKGExKUVdUXgFNEU0KSVMVR28MBQ0BUwJZBwxK')
+baselogin = s.get('https://www.dmm.com/my/-/login/=/path=Sg9VTQFXDFcXFl5bWlcKGExKUVdUXgFNEU0KSVMVR28MBQ0BUwJZBwxK')
 token = re.search('"token": "([a-f0-9]{32})"', baselogin.text)
 dmmt = re.search('"DMM_TOKEN", "([a-f0-9]{32})"', baselogin.text)
-# print(dmmt.group(1))
+s.cookies.set("ckcy","1", domain="www.dmm.com", path="/netgame/", expires="1549682583000")
 data = {'token' : token.group(1)}
 
 r = requests.Request("POST", "https://www.dmm.com/my/-/login/ajax-get-token/", data=data)
@@ -16,8 +18,74 @@ pretokens = s.prepare_request(r)
 pretokens.headers["DMM_TOKEN"] = dmmt.group(1)
 pretokens.headers["X-Requested-With"] = "XMLHttpRequest"
 tokens = s.send(pretokens)
-# {"token":"99fdc59426d71af002bf013c341ac378","login_id":"dc641a3f04d76d021f0716308e6d95f0","password":"18d7081099715c55e93bed1fb257cd2b"}
+agt = tokens.json()
+data = { \
+	"token" : agt["token"], \
+	"login_id" : config.get('dmm', 'user'), \
+	"save_login_id" : 0, \
+	"password" : config.get('dmm', 'pass'), \
+	"save_password" : 0, \
+	"use_auto_login" : 0, \
+	agt["login_id"] : config.get('dmm', 'user'), \
+	agt["password"] : config.get('dmm', 'pass'), \
+	"path" : 'Sg9VTQFXDFcXFl5bWlcKGExKUVdUXgFNEU0KSVMVR28MBQ0BUwJZBwxK', \
+	"prompt" : None, \
+	"client_id" : None, \
+	"display" : None \
+}
+auth = s.post("https://www.dmm.com/my/-/login/auth/", data=data)
+# go to app page now.
+basegame = s.get('http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/')
 f = open('./dump.txt', 'wb')
-f.write(baselogin.text.encode('utf-8'));
+f.write(basegame.content);
 f.close()
-print(tokens.text)
+viewerre = re.search('VIEWER_ID[^\d"]+([\d"]+)',basegame.text)
+viewid = viewerre.group(1)
+stre = re.search('(?: )+ST(?: )+(?:: )"([^\r\n"]+)',basegame.text)
+st = stre.group(1)
+dmmtime = math.floor(time.time() * 1000)
+
+data = { \
+	"refresh" : 3600, \
+	"url" : "http://203.104.209.7/kcsapi/api_world/get_id/" + viewid + "/1/" + str(dmmtime), \
+	"httpMethod" : "GET", \
+	"headers" : "", \
+	"postData" : "", \
+	"authz" : "", \
+	"st" : "", \
+	"contentType" : "JSON", \
+	"numEntries" : "3", \
+	"getSummaries" : "false", \
+	"signOwner" : "true", \
+	"signViewer" : "true", \
+	"gadget" : "http://203.104.209.7/gadget.xml", \
+	"container" : "dmm", \
+	"bypassSpecCache" : "", \
+	"getFullHeaders" : "false" \
+}
+worldform = s.get('http://osapi.dmm.com/gadgets/makeRequest?' + '&'.join(['%s=%s' % (key, value) for (key, value) in data.items()]))
+worldre = re.search(r'\\"api_world_id\\":(\d+)',worldform.text)
+servip = servers[int(worldre.group(1))-1]
+
+data = { \
+	"url" : "http://" + servip + "/kcsapi/api_auth_member/dmmlogin/" + viewid + "/1/" + str(dmmtime), \
+	"httpMethod" : "GET", \
+	"headers" : "", \
+	"postData" : "", \
+	"authz" : "signed", \
+	"st" : st, \
+	"contentType" : "JSON", \
+	"numEntries" : "3", \
+	"getSummaries" : "false", \
+	"signOwner" : "true", \
+	"signViewer" : "true", \
+	"gadget" : "http://203.104.209.7/gadget.xml", \
+	"container" : "dmm", \
+	"bypassSpecCache" : "", \
+	"oauthState" : "", \
+	"getFullHeaders" : "false" \
+}
+main = s.post("http://osapi.dmm.com/gadgets/makeRequest", data=data)
+tokenre = re.search(r'\\"api_token\\":\\"([0-9a-f]+)',main.text)
+token = tokenre.group(1)
+print("Your api link is : http://" + servip + "/kcs/mainD2.swf?api_token=" + token)
